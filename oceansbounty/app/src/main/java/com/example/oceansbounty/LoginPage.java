4ca3d91c27d5af93de37087f1be869bcbb4536d3
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2024-12-05 21:19:45 +0800

    Created files for login
    Added LoginPage.java and login_page.xml

---
+++ oceansbounty/app/src/main/java/com/example/oceansbounty/LoginPage.java
@@ -0,0 +1,7 @@
+package com.example.oceansbounty;
+
+public class LoginPage {
+}
+
+// make a java class full of UI components UIComponents.java
+// buttons and text fields can be manipulated better, like change how it looks on click
